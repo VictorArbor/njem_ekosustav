0236c5c791d4b3f0529ba5dfa5ad7ef67aab9107
VictorArbor <dr.victor.arbor@gmail.com>
2026-09-05 12:52:48 +0000

    Add Nix build pipeline for NJM OS

--- flake.nix
+++ flake.nix
@@ -1,16 +1,33 @@
 {
-  description = "NJM jezični procesor";
+  description = "NJM OS Pure-Java Android Build Environment";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    android-nixpkgs.url = "github:tadfisher/android-nixpkgs";
+  };
 
-  outputs = { self, nixpkgs }: {
-    devShells.aarch64-linux.default =
-      let pkgs = nixpkgs.legacyPackages.aarch64-linux;
-      in pkgs.mkShell {
-        packages = with pkgs; [
-          git
-          clang
+  outputs = { self, nixpkgs, android-nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      };
+      
+      sdk = android-nixpkgs.sdk.${system} (sdkPkgs: with sdkPkgs; [
+        build-tools-34-0-0
+        platforms-android-34
+        platform-tools
+      ]);
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = [
+          sdk
+          pkgs.jdk17
+          pkgs.zip
+          pkgs.apksigner
         ];
       };
-  };
+    };
 }
